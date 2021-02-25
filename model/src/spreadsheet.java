4aee0beecb31c4b7c9950b1bd4816f4e0b0eefa7
kevin <klian@colgate.edu>
2021-02-25 11:47:01 +0800

    [feat] 修改注释

--- model/src/spreadsheet.java
+++ model/src/spreadsheet.java
@@ -5,7 +5,7 @@ public class spreadsheet {
     /**
      * 该函数的目的是获取在column列的值，内容为等式的话需要计算并返回其结果。举例，若在"A1"列存储的值为"=7+3"，sheet.get("A1")应返回"10"。
      * @param column 列数
-     * @return 该列存的值
+     * @return 该列存储的值，默认为空字符串
      */
     public String get(String column){
         throw new NotImplementedException();
@@ -14,7 +14,7 @@ public class spreadsheet {
     /**
      * 该函数的目的是获取在column列的字符串值，内容为等式的话不需要计算，直接返回字符串。举例，若在"A1"列存储的值为"=7+3"，sheet.getLiteral("A1")应返回"=7+3"。
      * @param column 列数
-     * @return 该列存的值
+     * @return 该列存储的字符串值，默认为空字符串
      */
     public String getLiteral(String column){
         throw new NotImplementedException();
